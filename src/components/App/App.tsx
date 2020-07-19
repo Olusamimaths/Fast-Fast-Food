6eeff646b41c4a9546bdc88a77b40fdd02254702
Samuel Olusola <olusola.samuel.oluwatobi@gmail.com>
2020-07-19 15:03:43 +0100

    Structured folder

---
+++ src/components/App/App.tsx
@@ -0,0 +1,14 @@
+import React from "react";
+import Navbar from "../Navbar";
+import "../../styles/app.scss";
+
+function App(): JSX.Element {
+  return (
+    <div className="App">
+      <Navbar />
+      <h1>Hello Worl</h1>
+    </div>
+  );
+}
+
+export default App;
---
+++ src/components/Navbar/Navbar.tsx
@@ -0,0 +1,13 @@
+import React from "react";
+import Container from "react-bootstrap/Container";
+import Row from "react-bootstrap/Row";
+
+function navbar(): JSX.Element {
+  return (
+    <Container fluid>
+      <Row>Hello...</Row>
+    </Container>
+  );
+}
+
+export default navbar;
