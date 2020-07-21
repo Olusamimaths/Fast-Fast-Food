4c53b34bd1c8adaa9a6b6da4a1358d9f2681425d
Samuel Olusola <olusola.samuel.oluwatobi@gmail.com>
2020-07-21 06:44:04 +0100

    Typed FoodCard

--- src/components/Foods/FoodCard.tsx
+++ src/components/Foods/FoodCard.tsx
@@ -1,20 +1,30 @@
 import React from "react";
 import Card from "react-bootstrap/Card";
 import Button from "react-bootstrap/Button";
-import image1_xs from "../../images/image1_xs.jpg";
 import "../../styles/FoodCard.scss";
 
-const FoodCard: React.FC = () => {
+interface FoodCardProps {
+  image: string;
+  title: string;
+  description: string;
+  link: string;
+}
+
+const FoodCard: React.FC<FoodCardProps> = ({
+  image,
+  title,
+  description,
+  link,
+}) => {
   return (
     <Card className="mr-4 food-card">
-      <Card.Img variant="top" src={image1_xs} />
+      <Card.Img variant="top" src={image} />
       <Card.Body>
-        <Card.Title className="text-secondary">Card Title</Card.Title>
-        <Card.Text className="text-primary">
-          Some quick example text to build on the card title and make up the
-          bulk of the card's content.
-        </Card.Text>
-        <Button variant="secondary">Go somewhere</Button>
+        <Card.Title className="text-secondary">{title}</Card.Title>
+        <Card.Text className="text-primary">{description}</Card.Text>
+        <Button variant="secondary" href={link}>
+          Add
+        </Button>
       </Card.Body>
     </Card>
   );
---
+++ src/components/Foods/data.ts
@@ -0,0 +1,2 @@
+import image1_xs from "../../images/image1_xs.jpg";
+import food1 from "../../images/food1.jpg";
